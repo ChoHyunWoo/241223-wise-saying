ac2ba31a55d5ba7d079d5e064b6350220a670a15
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-02 10:01:43 +0900

    work31

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.util.Scanner;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         App app = new App();
--- src/main/java/wiseSaying/WiseSaying.java
+++ src/main/java/wiseSaying/WiseSaying.java
@@ -1,15 +1,23 @@
-package org.example;
+package wiseSaying;
 
 public class WiseSaying {
     private int id;
     private String content;
     private String author;
 
-    WiseSaying(int id,String content, String author){
+    public WiseSaying(int id, String content, String author){
         this.id = id;
         this.content = content;
         this.author=author;
     }
+    public void setContent(String content){
+        this.content = content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
     public int getId(){
         return id;
     }
